47b5ab3b5613e5607e457142072bf993c6ce670f
mahesh <glman74@gmail.com>
2013-05-10 18:53:46 +0530

    plotting a_x

--- showdata.py
+++ showdata.py
@@ -9,17 +9,46 @@
 ################################################################################
 
 import sys, serial
+import numpy as np
+from time import sleep
+from collections import deque
+from matplotlib import pyplot as plt
+
+maxLen = 100
+# data arrays
+ax = deque([0]*maxLen)
+ay = deque()
+az = deque()
+
+def updateData(line, xval):
+  if len(ax) < maxLen:
+    ax.append(xval)
+  else:
+    ax.pop()
+    ax.appendleft(xval)
+  line.set_ydata(ax)
+  plt.draw()
 
 # main() function
 def main():
   # use sys.argv if needed
-  print 'hello'
+
+  # plot parameters
+  plt.ion() # set plot to animated
+
+  # make plot
+  xline, = plt.plot(ax)
+  plt.ylim([-2.5,2.5])
+
+  print 'plotting MMA7660 data...'
   strPort = '/dev/tty.usbserial-A7006Yqh'
   ser = serial.Serial(strPort, 9600)
   while True:
     try:
-      line = ser.readline()
-      print line
+      line = ser.readline().split()
+      #print line[0]
+      #print line[1:]
+      updateData(xline, line[1])
     except KeyboardInterrupt:
       print 'exiting'
       break
